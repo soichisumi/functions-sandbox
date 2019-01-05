fd410418b2afcb8a6b788a5924fa743850961218
Soichi Sumi <soichi.sumi@gmail.com>
2019-01-05 10:44:46 +0900

    reformat

--- deploy-multiregion/func-jp/functions/index.js
+++ deploy-multiregion/func-jp/functions/index.js
@@ -1,8 +1,8 @@
 const functions = require('firebase-functions');
 
 exports.helloWorld = functions.region("asia-northeast1").https.onCall((data, ctx) => {
- return {
-     res: JSON.stringify(data),
-     region: "asia-northeast1"
- }
+    return {
+        res: JSON.stringify(data),
+        region: "asia-northeast1"
+    }
 });
